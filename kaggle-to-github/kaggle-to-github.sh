db7632f314a9e93b7493a98c8859ab94007a4f7b
adrien_debray <adrien@dataroots.io>
2023-06-27 12:12:54 +0200

    Added a new kernel

--- kaggle-to-github/kaggle-to-github.sh
+++ kaggle-to-github/kaggle-to-github.sh
@@ -1,12 +1,13 @@
 ROOT_INPUT=/kaggle/input
 NEW_INPUT=./kaggle/input
+
 KERNEL=$1
-#DATASET=$2
-REPO=$2
+KAGGLE_USERNAME=$2 
+
+REPO=vesuvius-ink-detection
 GITHUB_USERNAME=datarootsio
-KAGGLE_USERNAME=$3 # adriendebray
+
 echo "Kernel: " $KERNEL
-#echo "Dataset: " $DATASET
 echo "Repo: " $REPO
 echo "Kaggle Username: " $KAGGLE_USERNAME
 echo "Github Username: " $GITHUB_USERNAME
@@ -21,21 +22,10 @@ else
 fi
 cd ./$REPO
 echo "----Downloading Kernel to github repo----"
-#kaggle kernels pull -m $KAGGLE_USERNAME/$KERNEL -p ./$REPO 
-kaggle kernels pull adriendebray/resnet18d-inference -p ./notebooks
-#echo "----Downloading dataset to github repo----"
-#kaggle datasets download -d adriendebray/resnet18 -p ./$REPO/kaggle/input/$KERNEL/ --force  --unzip 
-#result=$(kaggle datasets download $DATASET -p ./$REPO/kaggle/input/$KERNEL/ --force  --unzip )
-#echo $result
-# cd ./$REPO
-#echo "----Create conda package list----"
-#conda env export -n kaggle -f packages.yml
+kaggle kernels pull $KAGGLE_USERNAME/$KERNEL -p ./notebooks 
+#kaggle kernels pull adriendebray/resnet18d-inference -p ./notebooks
 
-#echo "----Converting jupyter notebook to python code----"
-#jupyter nbconvert --to script *.ipynb
-#ls -a
-#echo "----Fixing dataset path----"
-#sed -i "s|$ROOT_INPUT|$NEW_INPUT|g" $KERNEL.py
 git stage --all
 git commit -m "Added a new kernel"
 git push
+cd ../kaggle-to-github
